bcb5a4b03dfaecdd30db3f2fc65a0d66b641e03f
engin can <altinozengincan@gmail.com>
2022-11-30 22:43:02 +0300

    Without Interfaceses

---
+++ Assets/Scripts/Car/Delivery.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] float delayTime = 0.5f;
+
+    [SerializeField] Color32 hasPackageColor = new Color32();
+    [SerializeField] Color32 hasNoPackageColor = new Color32();
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D gameObject)
+    {
+
+        // if we got the package
+        if (gameObject.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Picked up!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(gameObject.gameObject, delayTime);
+
+        }
+
+        // if we delivered the package
+        if (gameObject.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Delivered!");
+            spriteRenderer.color = hasNoPackageColor;
+            hasPackage = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Car/FallowCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject thingToFollow;
+    private void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}
